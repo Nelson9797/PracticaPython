036fb3c73d907ac8c23f102ffb86c193b2d8539c
BreadyDev <juantonioramos26@gmail.com>
2023-02-03 18:15:46 -0600

    Optimización de 01_Palindromas.py

--- 01_Palindromas.py
+++ 01_Palindromas.py
@@ -2,11 +2,7 @@
 
 palabra = str(input("Ingresa una palabra: ").lower().replace(" ", ""))
 i = len(palabra) - 1
-arbalap = ""
-
-while  i >= 0:
-    arbalap = arbalap + palabra[i]
-    i -= 1
+arbalap = palabra[::-1]
     
 if palabra == arbalap:
     print("La palabra es palindroma")
