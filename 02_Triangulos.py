ffd9f02aa8f8b05de221bbb69cd9fcfcaee41063
Nelson9797 <107153407+Nelson9797@users.noreply.github.com>
2023-02-28 15:45:08 -0500

    Update 02_Triangulos.py
    El lado del triangulo siempre es menor estricto que la suma de las otras 2

--- 02_Triangulos.py
+++ 02_Triangulos.py
@@ -2,7 +2,7 @@
 # Cualquier lado no debe de ser mas grande que la suma de los otros dos
 
 a, b, c = int(input("Ingresa el lado A: ")), int(input("Ingresa el lado C: ")), int(input("Ingresa el lado C: "))
-if((a <= (b + c)) and (b <= (a + c)) and (c <= (a + b))):
+if((a < (b + c)) and (b < (a + c)) and (c < (a + b))):
     print("El triangulo es valido")
 else:
     print("El triangulo es invalido")
